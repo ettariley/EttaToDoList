42d10dabcef13fa44e4379f673c240714df52c1f
Etta Baker <etta.r.baker@gmail.com>
2022-02-21 22:08:31 -0500

    Functionality to add new item to list

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 function newItem() {
+
+    //Adding to the new list of items
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        let list = $('#list');
+        list.append(li);
+    }
+
 }   
